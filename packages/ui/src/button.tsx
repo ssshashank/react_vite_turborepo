0d9f913be4027ad116bad04dcae09c005a5128e8
Shekhar S <shekharshashank97@gmail.com>
2023-12-10 13:46:28 +0530

    turborepo_vite_template

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,16 @@
 "use client";
-
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+	children: ReactNode;
+	className?: string;
+	appName: string;
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button className={className} onClick={() => console.log()}>
+			{children}
+		</button>
+	);
 };
---
+++ packages/ui/src/heading.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Heading = () => {
+	return <div>heading</div>;
+};
+
+export default Heading;
