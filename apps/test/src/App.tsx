46b4cb03c06bf4144b2b133ac4706f270a4c3e9e
Shekhar S <shekharshashank97@gmail.com>
2023-12-12 20:26:02 +0530

    ShadCN lib integration

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Heading from "@repo/ui/heading";
+import { Button } from "@repo/ui/shadcnButton";
 function App() {
 	const [count, setCount] = useState(0);
 
@@ -28,6 +29,7 @@ function App() {
 			<p className='read-the-docs'>
 				Click on the Vite and React logos to learn more
 			</p>
+			<Button>New ShadCN Button</Button>
 			<Heading />
 		</>
 	);
--- packages/ui/src/heading.tsx
+++ packages/ui/src/heading.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { Button } from "./components/shadcn/button";
+import { Card } from "./components/shadcn/card";
+
 const Heading = () => {
-	return <div className='text-3xl text-red-900'>heading</div>;
+	return (
+		<div className='text-3xl text-red-900'>
+			<Button>ShadCN Button</Button>
+			<Card>
+				<h1>Hello</h1>
+			</Card>
+		</div>
+	);
 };
 
 export default Heading;
